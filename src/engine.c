e099291fa5d3af6a9bd422b29cee44f8408c6939
Chenguang Wang <w3cing@gmail.com>
2011-12-05 20:53:50 -0800

    using shift to switch Icelandic/English character inputting.

--- src/engine.c
+++ src/engine.c
@@ -215,6 +215,7 @@ ibus_enchant_engine_process_key_event (IBusEngine *engine,
      IBusEnchantEngine *enchant = (IBusEnchantEngine *)engine;
 
      static gboolean _mkey_enabled = FALSE;
+     static gboolean _engl_input = FALSE;
 
      if (modifiers & IBUS_RELEASE_MASK)
           return FALSE;
@@ -240,6 +241,11 @@ ibus_enchant_engine_process_key_event (IBusEngine *engine,
      case IBUS_Escape:
           _mkey_enabled = FALSE;
           return FALSE;
+     case IBUS_Shift_L:
+     case IBUS_Shift_R:
+          _engl_input = !_engl_input;
+          _mkey_enabled = FALSE;
+          return TRUE;
      }
 
      // a b c e d f g h i j k l m n o p <MODIFY-KEY> q  r s t u v w x y z
@@ -247,36 +253,42 @@ ibus_enchant_engine_process_key_event (IBusEngine *engine,
           gboolean _is_capital = TRUE;
           if('a' <= keyval && keyval <= 'z')
                _is_capital = FALSE;
+
+          if(_engl_input) {
+               g_string_insert_unichar(enchant->preedit, enchant->cursor_pos, keyval);
+          } else {
           
-          if(keyval == 'q' || keyval == 'Q') {
-               _mkey_enabled = !_mkey_enabled;
-               return TRUE;
-          }
+               if(keyval == 'q' || keyval == 'Q') {
+                    _mkey_enabled = !_mkey_enabled;
+                    return TRUE;
+               }
 
-          // a b æ d e f g h i j k l m n o p <M> r s t u v ö x y ð á é í ó ú ý þ
-          int _l_map[] = {97, 98, 230, 100, 101, 102, 103, 104, 105, 106, 107, 108,
-                          109, 110, 111, 112, 95, 114, 115, 116, 117, 118, 246, 120,
-                          121, 240, 225, 233, 237, 243, 250, 253, 254};
-          // A B Æ D E F G H I J K L M N O P <M> R S T U V Ö X Y Ð Á É Í Ó Ú Ý Þ
-          int _u_map[] = {65, 66, 198, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78,
-                          79, 80, 95, 82, 83, 84, 85, 86, 214, 88, 89, 208, 193, 201,
-                          205, 211, 218, 221, 222};
+               // a b æ d e f g h i j k l m n o p <M> r s t u v ö x y ð á é í ó ú ý þ
+               int _l_map[] = {97, 98, 230, 100, 101, 102, 103, 104, 105, 106, 107, 108,
+                               109, 110, 111, 112, 95, 114, 115, 116, 117, 118, 246, 120,
+                               121, 240, 225, 233, 237, 243, 250, 253, 254};
+               // A B Æ D E F G H I J K L M N O P <M> R S T U V Ö X Y Ð Á É Í Ó Ú Ý Þ
+               int _u_map[] = {65, 66, 198, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78,
+                               79, 80, 95, 82, 83, 84, 85, 86, 214, 88, 89, 208, 193, 201,
+                               205, 211, 218, 221, 222};
 
-          if(_mkey_enabled) {
-               _mkey_enabled = FALSE;
-               char _mkey_list[] = "aeiouyt";
-               int i;
-               for(i = 0; i < 7; i++) {
-                    if(keyval == _mkey_list[i] || keyval == _mkey_list[i] - 'a' + 'A') {
-                         keyval = 26 + i + (_is_capital ? 'A' : 'a');
+               if(_mkey_enabled) {
+                    _mkey_enabled = FALSE;
+                    char _mkey_list[] = "aeiouyt";
+                    int i;
+                    for(i = 0; i < 7; i++) {
+                         if(keyval == _mkey_list[i] || keyval == _mkey_list[i] - 'a' + 'A') {
+                              keyval = 26 + i + (_is_capital ? 'A' : 'a');
+                         }
                     }
                }
-          }
 
-          if(_is_capital) {
-               g_string_insert_unichar(enchant->preedit, enchant->cursor_pos, _u_map[keyval - 'A']);
-          } else {
-               g_string_insert_unichar(enchant->preedit, enchant->cursor_pos, _l_map[keyval - 'a']);
+               if(_is_capital) {
+                    g_string_insert_unichar(enchant->preedit, enchant->cursor_pos, _u_map[keyval - 'A']);
+               } else {
+                    g_string_insert_unichar(enchant->preedit, enchant->cursor_pos, _l_map[keyval - 'a']);
+               }
+
           }
 
           enchant->cursor_pos++;
